0f5a9d89712fc1d9172452b37335b56ff443e71d
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-03-29 20:00:46 +0800

    start project

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
 "use strict";
 
-let strr = 'ffdddbbb';
-let reversStr = strr.split('');
-console.log(reversStr);
-let reversStr1 = strr.split('').reverse();
-console.log(reversStr1);
-let reversStr2 = strr.split('').reverse().join('');
-console.log(reversStr2)
