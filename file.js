eed394c25c44deaf5a8bc72c6ce5fce96daf68e9
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-03-31 13:36:40 +0800

    Complicated task

--- file.js
+++ file.js
@@ -1,3 +1,13 @@
 let num = 266219;
 let result = ('' + num).split('');
 console.log(result);
+let count = 1;
+for (let i of result) {
+  count *= parseInt(i);
+}
+
+let countSecond = count * count * count;
+let firstTwoNumber = Number(String(count).slice(0, 2));
+console.log(firstTwoNumber);
+console.log(countSecond);
+console.log(count);
