1ffdde28db5b2a4628bba263c56ade086b22957e
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-03-31 11:38:08 +0800

    create lesson01

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 "use strict";
+let title = 'Super project';
+let screens = 'Simple,Complex,Interactive';
+let screenPrice = 8;
+let rollback = 93;
+let fullPrice = 80000;
+let adaptive = true;
 
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.toLocaleLowerCase().split())
+
+console.log(screens.length);
+console.log((fullPrice(rollback / 100)));
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
