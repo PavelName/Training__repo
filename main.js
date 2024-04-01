619f9eac62c17f2fd9e405bd540e514c18828aa7
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-04-01 14:35:38 +0800

    created a construction with conditions

--- main.js
+++ main.js
@@ -11,5 +11,18 @@ let service2 = prompt('Какой дополнительный тип услуг
 let servicePrice2 = +prompt('Сколько это будет стоить?');
 
 let fullPrice = (screenPrice + servicePrice1 + servicePrice2);
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * 0.5));
+
+if (fullPrice  >= 30000) {
+  alert("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  alert("Даем скидку в 5%");
+} else if ( fullPrice < 15000 && fullPrice >= 0) {
+  alert('Скидка не предусмотрена');
+} else if (fullPrice < 0) {
+  alert('Что то пошло не так');
+};
+
+
 console.log(fullPrice);
-let servicePercentPrice = 
+console.log(servicePercentPrice);
