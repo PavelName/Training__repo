226b8f50316d689872607a53a0c8ba0891ba9a24
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-04-10 11:01:46 +0800

    adaptive

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?','Простые, Сложные, Интерактивные' );
 
 let screenPrice = +prompt('Сколько будет стоить данная работа?', 12000);
+let rollback = 15;
 let adaptive = confirm('Нужен ли адаптив на сайте?');
 
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
@@ -12,6 +13,7 @@ let servicePrice2 = +prompt('Сколько это будет стоить?');
 
 let fullPrice = (screenPrice + servicePrice1 + servicePrice2);
 let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * 0.5));
+
 
 if (fullPrice  >= 30000) {
   alert("Даем скидку в 10%");
@@ -23,6 +25,13 @@ if (fullPrice  >= 30000) {
   alert('Что то пошло не так');
 };
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice}  рублей/ долларов/гривен/юани `)
 
-console.log(fullPrice);
+console.log(screens.toLowerCase().split(','));
+console.log(screens.length);
 console.log(servicePercentPrice);
+console.log(fullPrice * (rollback / 100));
