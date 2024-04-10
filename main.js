e929096b33778ad6030f93408629a98d3deafb62
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-04-10 22:23:53 +0800

    lesson04

--- main.js
+++ main.js
@@ -11,27 +11,58 @@ let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
 
-let fullPrice = (screenPrice + servicePrice1 + servicePrice2);
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * 0.5));
+let fullPrice;
+let allServicePrices;
 
+let servicePercentPrice;
 
-if (fullPrice  >= 30000) {
-  alert("Даем скидку в 10%");
-} else if (fullPrice >= 15000 && fullPrice < 30000) {
-  alert("Даем скидку в 5%");
-} else if ( fullPrice < 15000 && fullPrice >= 0) {
-  alert('Скидка не предусмотрена');
-} else if (fullPrice < 0) {
-  alert('Что то пошло не так');
+const chowTypeOf = function (varible){
+  console.log(varible, typeof varible)
+};
+const getAllServicePrices = function() {
+  return screenPrice + servicePrice1 + servicePrice2;
+};
+const getTitle = function() {
+  return title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase;
 };
 
+const getServicePercentPrices = function() {
+  return Math.ceil(fullPrice * (rollback / 100));
+}
+function getFullPrice () {
+  return screenPrice + allServicePrices;
+}
+const getRoiibackMessage = function (price){
+  if (price  >= 30000) {
+    return "Даем скидку в 10%";
+  } else if (price >= 15000 && price < 30000) {
+    return "Даем скидку в 5%";
+  } else if ( price < 15000 && price >= 0) {
+    return 'Скидка не предусмотрена';
+  } else if (price < 0) {
+    return 'Что то пошло не так';
+  };
+}
+
+chowTypeOf(title);
+chowTypeOf(fullPrice);
+chowTypeOf(adaptive);
+
+getTitle();
+
+ allServicePrices = getAllServicePrices();
+ fullPrice = getFullPrice();
+ servicePercentPrice = getServicePercentPrices();
+
+console.log(getRoiibackMessage(fullPrice));
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
+
 console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
-console.log(`Стоимость разработки сайта ${fullPrice}  рублей/ долларов/гривен/юани `)
+console.log(`Стоимость разработки сайта ${fullPrice}  рублей/ долларов/гривен/юани `);
 
 console.log(screens.toLowerCase().split(','));
 console.log(screens.length);
 console.log(servicePercentPrice);
-console.log(fullPrice * (rollback / 100));
+
